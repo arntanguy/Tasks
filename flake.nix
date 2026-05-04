a09fd7a4e4a47c32ba3b53b94f3cb4ed9a68ce1c
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-05-04 13:11:19 +0200

    nix: use rbdyn nanobind pr as an overlay

--- flake.nix
+++ flake.nix
@@ -1,14 +1,23 @@
 {
   description = "Nix flake for Tasks";
   inputs.mc-rtc-nix.url = "github:mc-rtc/nixpkgs";
+  inputs.rbdyn.url = "github:jrl-umi3218/RBDyn/pull/138/head";
+  # inputs.rbdyn.url = "path:/home/arnaud/devel/mc-rtc-nix/workspace/RBDyn";
 
   outputs =
     inputs:
     inputs.mc-rtc-nix.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        overrideAttrs.tasks = {
+        overlays = [
+          inputs.rbdyn.overlays.flakoboros
+        ];
+
+        overrideAttrs.tasks = {pkgs-final, drv-prev, ...}: {
           src = lib.cleanSource ./.;
+          nativeBuildInputs = with pkgs-final; [
+            jrl-cmakemodules
+          ] ++ drv-prev.nativeBuildInputs;
         };
       }
     );
